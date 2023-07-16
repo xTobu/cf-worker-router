f2329fbd67864cebe3c5bb32b77ee37e4b7471b3
xTobu <junxiang.oriint@gmail.com>
2023-07-16 16:00:56 +0800

    update CORS

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,21 @@
-import { Router } from 'itty-router';
-
+import { Router, createCors } from 'itty-router';
+const { preflight, corsify } = createCors({
+	methods: ['GET', 'PATCH', 'POST'],
+	origins: ['http://127.0.0.1:5173'],
+	headers: {
+		'my-funky-header': 'is pretty funky indeed',
+	},
+});
 // now let's create a router (note the lack of "new")
 const router = Router();
-
+router.all('*', preflight);
 // GET collection index
 router.get('/api/todos', () => new Response('Todos Index!'));
 
 // GET item
-router.get('/api/todos/:id', ({ params }) => new Response(`Todo #${params.id}`));
+router.get('/api/todos/:id', ({ params }) => {
+	return corsify(new Response(`Todo #${params.id}`));
+});
 
 // POST to the collection (we'll use async here)
 router.post('/api/todos', async (request) => {
